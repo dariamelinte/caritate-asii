42e623a0b757a7f59d66d9cb90928faa615edbc4
daria melinte <dariamelinte2003@gmail.com>
2021-11-24 18:55:57 +0200

    update partners list

--- js/partners.js
+++ js/partners.js
@@ -1,28 +1,53 @@
 const partners = [
   {
     id: '1',
-    imageUrl: 'https://picsum.photos/200',
-    ulr: 'https://picsum.photos/200'
+    imageUrl: '../assets/images/partners/softvision.png',
+    ulr: 'https://www.cognizantsoftvision.com/'
   },
   {
     id: '2',
-    imageUrl: 'https://picsum.photos/200',
-    ulr: 'https://picsum.photos/200'
+    imageUrl: '../assets/images/partners/3pillar.png',
+    ulr: 'https://www.3pillarglobal.com/'
   },
   {
     id: '3',
-    imageUrl: 'https://picsum.photos/200',
-    ulr: 'https://picsum.photos/200'
+    imageUrl: '../assets/images/partners/beenear.png',
+    ulr: 'https://www.beenear.com/'
   },
   {
     id: '4',
-    imageUrl: 'https://picsum.photos/200',
-    ulr: 'https://picsum.photos/200'
+    imageUrl: '../assets/images/partners/bytex.png',
+    ulr: 'https://bytex.net/'
   },
   {
     id: '5',
-    imageUrl: 'https://picsum.photos/200',
-    ulr: 'https://picsum.photos/200'
+    imageUrl: '../assets/images/partners/deloitte.png',
+    ulr: 'https://ro.deloittedigital.com/'
+  },
+  {
+    id: '6',
+    imageUrl: '../assets/images/partners/levi.png',
+    ulr: 'https://www.levi9.com/'
+  },
+  {
+    id: '7',
+    imageUrl: '../assets/images/partners/as.png',
+    ulr: 'https://careers.amplified.software/'
+  },
+  {
+    id: '8',
+    imageUrl: '../assets/images/partners/iasi_ai.png',
+    ulr: 'https://iasi.ai/'
+  },
+  {
+    id: '9',
+    imageUrl: '../assets/images/partners/wink.png',
+    ulr: 'https://wink.ro/'
+  },
+  {
+    id: '10',
+    imageUrl: '../assets/images/partners/ziarul_iasi.png',
+    ulr: 'https://www.ziaruldeiasi.ro/'
   },
 ];
 
