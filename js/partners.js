f7f63cb23a2481fa20e8a52ed64131f018fd1095
daria melinte <dariamelinte2003@gmail.com>
2021-12-14 00:14:33 +0200

    change styling pattern

--- js/partners.js
+++ js/partners.js
@@ -1,53 +1,13 @@
 const partners = [
   {
     id: '1',
-    imageUrl: 'assets/images/partners/softvision.png',
-    ulr: 'https://www.cognizantsoftvision.com/'
+    imageUrl: 'assets/images/partners/lliw.png',
+    ulr: 'https://m.facebook.com/lliw.coffee/?ref=py_c'
   },
   {
     id: '2',
-    imageUrl: 'assets/images/partners/3pillar.png',
-    ulr: 'https://www.3pillarglobal.com/'
-  },
-  {
-    id: '3',
-    imageUrl: 'assets/images/partners/beenear.png',
-    ulr: 'https://www.beenear.com/'
-  },
-  {
-    id: '4',
-    imageUrl: 'assets/images/partners/bytex.png',
-    ulr: 'https://bytex.net/'
-  },
-  {
-    id: '5',
-    imageUrl: 'assets/images/partners/deloitte.png',
-    ulr: 'https://ro.deloittedigital.com/'
-  },
-  {
-    id: '6',
-    imageUrl: 'assets/images/partners/levi.png',
-    ulr: 'https://www.levi9.com/'
-  },
-  {
-    id: '7',
-    imageUrl: 'assets/images/partners/as.png',
-    ulr: 'https://careers.amplified.software/'
-  },
-  {
-    id: '8',
-    imageUrl: 'assets/images/partners/iasi_ai.png',
-    ulr: 'https://iasi.ai/'
-  },
-  {
-    id: '9',
-    imageUrl: 'assets/images/partners/wink.png',
-    ulr: 'https://wink.ro/'
-  },
-  {
-    id: '10',
-    imageUrl: 'assets/images/partners/ziarul_iasi.png',
-    ulr: 'https://www.ziaruldeiasi.ro/'
+    imageUrl: 'assets/images/partners/agora.png',
+    ulr: 'https://agora-centrul-de-evenimente.business.site/'
   },
 ];
 
